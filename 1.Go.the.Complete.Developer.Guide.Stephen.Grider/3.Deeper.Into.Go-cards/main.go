6e8c2e8a7f69c709d6f6839ac83cffecd0d940d7
Hector <hmreluz@gmail.com>
2021-07-17 21:21:37 +1000

    Go to Complete Developer Guide - Stephen Grider - cards - Custom Type declarations

--- 1.Go.the.Complete.Developer.Guide.Stephen.Grider/3.Deeper.Into.Go-cards/main.go
+++ 1.Go.the.Complete.Developer.Guide.Stephen.Grider/3.Deeper.Into.Go-cards/main.go
@@ -1,17 +1,9 @@
 package main
 
-import "fmt"
-
 func main() {
-	cards := []string{"Ace of Diamonds", newCard()}
+	cards := deck{"Ace of Diamonds", newCard()}
 	cards = append(cards, "Six of Spades")
-
-	for i, card := range cards {
-		fmt.Println(i, card)
-
-	}
-
-	// fmt.Println(cards)
+	cards.print()
 }
 
 func newCard() string {
