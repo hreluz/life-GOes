7d7c9bd31e1021b7521c412c65db47a6b5faab00
Hector <hmreluz@gmail.com>
2021-07-19 13:30:25 +1000

    Go to Complete Developer Guide - Stephen Grider - cards - Shuffling a Deck

--- 1.Go.the.Complete.Developer.Guide.Stephen.Grider/3.Deeper.Into.Go-cards/deck.go
+++ 1.Go.the.Complete.Developer.Guide.Stephen.Grider/3.Deeper.Into.Go-cards/deck.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"math/rand"
 	"os"
 	"strings"
 )
@@ -60,4 +61,13 @@ func newDeckFromFile(filename string) deck {
 
 	s := strings.Split(string(bs), ",")
 	return deck(s)
+}
+
+//receiver
+func (d deck) shuffle() {
+	for i := range d {
+		newPosition := rand.Intn(len(d) - 1)
+
+		d[i], d[newPosition] = d[newPosition], d[i]
+	}
 }
