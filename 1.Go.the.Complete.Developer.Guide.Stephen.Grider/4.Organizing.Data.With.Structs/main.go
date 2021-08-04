7bcca5b8c35ac6661877391403fc7a5180e1b202
Hector <hmreluz@gmail.com>
2021-08-04 23:19:34 +1000

    Go to Complete Developer Guide - Stephen Grider - structs - embedding structs

--- 1.Go.the.Complete.Developer.Guide.Stephen.Grider/4.Organizing.Data.With.Structs/main.go
+++ 1.Go.the.Complete.Developer.Guide.Stephen.Grider/4.Organizing.Data.With.Structs/main.go
@@ -2,20 +2,37 @@ package main
 
 import "fmt"
 
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
 type person struct {
 	firstName string
 	lastName  string
+	contact   contactInfo
 }
 
 func main() {
 	// alex := person{firstName: "Alex", lastName: "Anderson"}
 	// fmt.Println(alex)
 
-	var alex person
+	// var alex person
 
-	alex.firstName = "Alex"
-	alex.lastName = "Anderson"
+	// alex.firstName = "Alex"
+	// alex.lastName = "Anderson"
 
-	fmt.Println(alex)
-	fmt.Printf("%+v", alex)
+	// fmt.Println(alex)
+	// fmt.Printf("%+v", alex)
+
+	jim := person{
+		firstName: "Jim",
+		lastName:  "Harpert",
+		contact: contactInfo{
+			email:   "jim@harpert.com",
+			zipCode: 833353,
+		},
+	}
+
+	fmt.Printf("%+v", jim)
 }
