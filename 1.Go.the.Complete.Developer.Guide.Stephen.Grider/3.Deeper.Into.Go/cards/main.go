bad1c8b4ed6a1531be2653a3ed465887a16f3f05
Hector <hmreluz@gmail.com>
2022-11-12 10:52:52 -0500

    1.Go-Stephen-Grider: 3.Depper.Into.Go : Custom Type declaration

--- 1.Go.the.Complete.Developer.Guide.Stephen.Grider/3.Deeper.Into.Go/cards/main.go
+++ 1.Go.the.Complete.Developer.Guide.Stephen.Grider/3.Deeper.Into.Go/cards/main.go
@@ -1,18 +1,14 @@
 package main
 
-import "fmt"
-
 //Array is a fixed number
 //Slice is an array without length
 
 func main() {
-	cards := []string{"Ace of Diamonds", newCard()}
+	cards := deck{"Ace of Diamonds", newCard()}
 
 	cards = append(cards, "Six of Spades")
 
-	for i, card := range cards {
-		fmt.Println(i, card)
-	}
+	cards.print()
 
 }
 
