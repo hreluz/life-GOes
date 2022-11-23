2570714d83b978e16da155ef80cfce594d650302
Hector <hmreluz@gmail.com>
2022-11-23 14:55:28 -0500

    1.Go-Stephen-Grider: 4.Structs: Updating Struct Values

--- 1.Go.the.Complete.Developer.Guide.Stephen.Grider/4.Structs/main.go
+++ 1.Go.the.Complete.Developer.Guide.Stephen.Grider/4.Structs/main.go
@@ -9,7 +9,21 @@ type person struct {
 
 func main() {
 	// alex := person{"Alex", "Anderson"}
-	alex := person{firstName: "Alex", lastName: "Anderson"}
-	fmt.Println(alex.firstName)
+	// alex := person{firstName: "Alex", lastName: "Anderson"}
+	// fmt.Println(alex.firstName)
+	var alex person
+
+	alex.firstName = "Alex"
+	alex.lastName = "Anderson"
+
+	fmt.Println(alex)
+	fmt.Printf("%+v", alex)
 
 }
+
+// When no assigning values there are zero values
+// TYPE 	ZERO VALUE
+// STRING 	""
+// INT 	0
+// FLOAT	0
+// BOOL 	FALSE
