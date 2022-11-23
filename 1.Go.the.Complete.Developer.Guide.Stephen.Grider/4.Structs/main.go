9db8377803396c2f2b53c159c9ee224030201f3e
Hector <hmreluz@gmail.com>
2022-11-23 17:10:11 -0500

    1.Go-Stephen-Grider: 4.Structs: Embedding Structs

--- 1.Go.the.Complete.Developer.Guide.Stephen.Grider/4.Structs/main.go
+++ 1.Go.the.Complete.Developer.Guide.Stephen.Grider/4.Structs/main.go
@@ -2,22 +2,39 @@ package main
 
 import "fmt"
 
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
 type person struct {
 	firstName string
 	lastName  string
+	contact   contactInfo
 }
 
 func main() {
 	// alex := person{"Alex", "Anderson"}
 	// alex := person{firstName: "Alex", lastName: "Anderson"}
 	// fmt.Println(alex.firstName)
-	var alex person
+	// var alex person
 
-	alex.firstName = "Alex"
-	alex.lastName = "Anderson"
+	// alex.firstName = "Alex"
+	// alex.lastName = "Anderson"
 
-	fmt.Println(alex)
-	fmt.Printf("%+v", alex)
+	// fmt.Println(alex)
+	// fmt.Printf("%+v", alex)
+
+	jim := person{
+		firstName: "Jim",
+		lastName:  "Party",
+		contact: contactInfo{
+			email:   "jim@party.com",
+			zipCode: 9400,
+		},
+	}
+
+	fmt.Printf("%+v", jim)
 
 }
 
