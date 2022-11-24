1b07a7c14596d73bf9d4d8595e0dea45ec75db54
Hector <hmreluz@gmail.com>
2022-11-23 23:29:42 -0500

    1.Go-Stephen-Grider: 4.Structs: Pointer Operations

--- 1.Go.the.Complete.Developer.Guide.Stephen.Grider/4.Structs/main.go
+++ 1.Go.the.Complete.Developer.Guide.Stephen.Grider/4.Structs/main.go
@@ -40,9 +40,16 @@ func main() {
 	jim.print()
 }
 
+// * person  = this is a type desc . It means we are working with a pointer to a person
 func (pointerToPerson *person) updateName(newFirstNaame string) {
 	(*pointerToPerson).firstName = newFirstNaame
+	// *pointerToPerson - This is an operator - it means we want to manipulate the value the pointer is referencing
 }
+
+// Pointers
+// 0001 (address) =  person {fistName: "Jim"} (value)
+// Turn address into a value with  *address
+// Turn value into address with &value
 
 //receiver
 func (p person) print() {
