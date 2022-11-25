705666be4fdfcdd13a13e1edbdb5d7c795120125
Hector <hmreluz@gmail.com>
2022-11-25 17:56:43 -0500

    1.Go-Stephen-Grider: 5.Map: Manipulating Maps

--- 1.Go.the.Complete.Developer.Guide.Stephen.Grider/5.Map/main.go
+++ 1.Go.the.Complete.Developer.Guide.Stephen.Grider/5.Map/main.go
@@ -3,10 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	colors := map[string]string{
-		"red":   "#ff0000",
-		"green": "#4bf756",
-	}
+	// colors := map[string]string{
+	// 	"red":   "#ff0000",
+	// 	"green": "#4bf756",
+	// }
+	// colors := map[string]string{}
+	// colors := make(map[string]string)
+	colors := make(map[int]string)
+
+	colors[10] = "#ffff"
+	colors[20] = "#00000"
+
+	delete(colors, 10)
 
 	fmt.Println(colors)
 }
