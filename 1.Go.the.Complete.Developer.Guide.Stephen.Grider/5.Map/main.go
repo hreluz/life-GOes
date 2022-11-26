545a220a3cdb38fd70fa1bf4d6376dbb21ec65de
Hector <hmreluz@gmail.com>
2022-11-25 22:19:53 -0500

    1.Go-Stephen-Grider: 6.Iterating over maps

--- 1.Go.the.Complete.Developer.Guide.Stephen.Grider/5.Map/main.go
+++ 1.Go.the.Complete.Developer.Guide.Stephen.Grider/5.Map/main.go
@@ -3,18 +3,26 @@ package main
 import "fmt"
 
 func main() {
-	// colors := map[string]string{
-	// 	"red":   "#ff0000",
-	// 	"green": "#4bf756",
-	// }
+	colors := map[string]string{
+		"red":   "#ff0000",
+		"green": "#4bf756",
+		"white": "#fffffff",
+	}
 	// colors := map[string]string{}
 	// colors := make(map[string]string)
-	colors := make(map[int]string)
+	// colors := make(map[int]string)
 
-	colors[10] = "#ffff"
-	colors[20] = "#00000"
+	// colors[10] = "#ffff"
+	// colors[20] = "#00000"
 
-	delete(colors, 10)
+	// delete(colors, 10)
 
-	fmt.Println(colors)
+	// fmt.Println(colors)
+	printMap(colors)
+}
+
+func printMap(c map[string]string) {
+	for color, hex := range c {
+		fmt.Println("Hex code for", color, "is", hex)
+	}
 }
