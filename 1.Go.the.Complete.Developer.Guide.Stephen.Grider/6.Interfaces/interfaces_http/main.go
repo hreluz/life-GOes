c521a911d25d0cffdeeb19c46e2c7e85b20922eb
Hector <hmreluz@gmail.com>
2022-12-23 17:19:41 -0500

    1.Go-Stephen-Grider: 6.Interfaces: custom writer

--- 1.Go.the.Complete.Developer.Guide.Stephen.Grider/6.Interfaces/interfaces_http/main.go
+++ 1.Go.the.Complete.Developer.Guide.Stephen.Grider/6.Interfaces/interfaces_http/main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 )
+
+type logWriter struct{}
 
 func main() {
 	resp, err := http.Get("http://google.com")
@@ -13,9 +16,20 @@ func main() {
 		os.Exit(1)
 	}
 
-	bs := make([]byte, 99999)
-	resp.Body.Read(bs)
+	// bs := make([]byte, 99999)
+	// resp.Body.Read(bs)
+	// fmt.Println(string(bs))
+
+	lw := logWriter{}
+
+	// io.Copy(os.Stdout, resp.Body)
+	io.Copy(lw, resp.Body)
+}
+
+func (logWriter) Write(bs []byte) (int, error) {
 	fmt.Println(string(bs))
+	fmt.Println("Just wrote this many bytes: ", len(bs))
+	return len(bs), nil
 }
 
 // https://pkg.go.dev/net/http@go1.19.3#Get
