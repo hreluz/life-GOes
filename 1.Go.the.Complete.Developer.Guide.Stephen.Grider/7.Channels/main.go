0b9f3b47a52aebd17240ab1c5ecf85da55d264d8
Hector <hmreluz@gmail.com>
2022-12-24 17:28:30 -0500

    1.Go-Stephen-Grider: 7.Channels: Function Literal

--- 1.Go.the.Complete.Developer.Guide.Stephen.Grider/7.Channels/main.go
+++ 1.Go.the.Complete.Developer.Guide.Stephen.Grider/7.Channels/main.go
@@ -22,8 +22,10 @@ func main() {
 	}
 
 	for l := range c {
-		time.Sleep(time.Second * 5)
-		go checkLink(l, c)
+		go func(link string) {
+			time.Sleep(time.Second * 5)
+			checkLink(link, c)
+		}(l)
 	}
 }
 
