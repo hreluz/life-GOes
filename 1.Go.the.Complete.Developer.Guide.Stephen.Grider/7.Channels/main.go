b48e1d209ef0e427a3428b766a2938c5dd44e471
Hector <hmreluz@gmail.com>
2022-12-24 17:07:44 -0500

    1.Go-Stephen-Grider: 7.Channels: Sleeping a Routine

--- 1.Go.the.Complete.Developer.Guide.Stephen.Grider/7.Channels/main.go
+++ 1.Go.the.Complete.Developer.Guide.Stephen.Grider/7.Channels/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func main() {
@@ -21,6 +22,7 @@ func main() {
 	}
 
 	for l := range c {
+		time.Sleep(time.Second * 5)
 		go checkLink(l, c)
 	}
 }
