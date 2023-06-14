a4e5b44e90025ef65f7882a8db730305e7f373a2
Hector <hmreluz@gmail.com>
2023-06-14 07:04:07 -0500

    2.Academind Go Complete Guide: variables

--- 2.Academind.Go.Complete.Guide/firstapp.go
+++ 2.Academind.Go.Complete.Guide/firstapp.go
@@ -3,5 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World!")
+	// var greetingText string
+	// greetingText = "Hello World!"
+
+	// var greetingText string = "Hello World"
+
+	greetingText := "Hello World"
+
+	fmt.Println(greetingText)
+	fmt.Println(greetingText)
 }
