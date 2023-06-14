df824941b5035b5d85b5f6c9d85294c048be0a05
Hector <hmreluz@gmail.com>
2023-06-14 07:10:21 -0500

    2.Academind Go Complete Guide: int

--- 2.Academind.Go.Complete.Guide/firstapp.go
+++ 2.Academind.Go.Complete.Guide/firstapp.go
@@ -9,7 +9,14 @@ func main() {
 	// var greetingText string = "Hello World"
 
 	greetingText := "Hello World"
+	luckyNumber := 17
+	evenMoreLuckyNumber := luckyNumber + 5
 
 	fmt.Println(greetingText)
 	fmt.Println(greetingText)
+	fmt.Println(luckyNumber)
+	fmt.Println(evenMoreLuckyNumber)
+
+	evenMoreLuckyNumber = luckyNumber * 3
+	fmt.Println(evenMoreLuckyNumber)
 }
