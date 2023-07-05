c4623c73700cff6f7d351b04ac51daa737467de3
Hector <hmreluz@gmail.com>
2023-07-04 22:28:18 -0500

    2.Academind Go Complete Guide: clearing and parsing user input

--- 2.Academind.Go.Complete.Guide/2.BMI.Calculator/bmii.go
+++ 2.Academind.Go.Complete.Guide/2.BMI.Calculator/bmii.go
@@ -4,6 +4,8 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
 
 var reader = bufio.NewReader(os.Stdin)
@@ -18,6 +20,13 @@ func main() {
 	fmt.Print("Please enter your height (m): ")
 	heightInput, _ := reader.ReadString('\n')
 
-	fmt.Print(weightInput)
-	fmt.Print(heightInput)
+	weightInput = strings.Replace(weightInput, "\n", "", -1)
+	heightInput = strings.Replace(heightInput, "\n", "", -1)
+
+	weight, _ := strconv.ParseFloat(weightInput, 64)
+	height, _ := strconv.ParseFloat(heightInput, 64)
+
+	fmt.Print(weight)
+	fmt.Print(height)
+
 }
