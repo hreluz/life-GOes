f313418618b8e7e34a7f6c1d81ad17ca63fcf7b4
Hector <hmreluz@gmail.com>
2023-07-04 22:11:51 -0500

    2.Academind Go Complete Guide: fetching user input

--- 2.Academind.Go.Complete.Guide/2.BMI.Calculator/bmii.go
+++ 2.Academind.Go.Complete.Guide/2.BMI.Calculator/bmii.go
@@ -1,11 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+var reader = bufio.NewReader(os.Stdin)
 
 func main() {
 	fmt.Println("BMI CALCULATOR")
 	fmt.Println("-------------------")
 
 	fmt.Print("Please enter your weight (kg): ")
+	weightInput, _ := reader.ReadString('\n')
+
 	fmt.Print("Please enter your height (m): ")
+	heightInput, _ := reader.ReadString('\n')
+
+	fmt.Print(weightInput)
+	fmt.Print(heightInput)
 }
