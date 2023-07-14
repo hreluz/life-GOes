b398e120e527bae61ddc3a02708acec5775fb74d
Hector <hmreluz@gmail.com>
2023-07-13 19:37:40 -0500

    2.Academind Go Complete Guide: working with slices

--- 2.Academind.Go.Complete.Guide/6.Arrays-Slices-and-Maps/lists.go
+++ 2.Academind.Go.Complete.Guide/6.Arrays-Slices-and-Maps/lists.go
@@ -14,6 +14,20 @@ func main() {
 
 	prices := [4]float64{4.3, 2.3, 1.0, 99.3}
 	fmt.Println(prices)
+
+	productNames[2] = "A carpet"
+
 	fmt.Println(productNames)
 	fmt.Println(prices[1])
+
+	// first value is
+	featurePrices := prices[1:3]
+	fmt.Println(featurePrices)
+
+	featurePrices2 := prices[:4]
+	fmt.Println(featurePrices2)
+
+	highlightedPrices := featurePrices2[:2]
+	fmt.Println(highlightedPrices)
+
 }
