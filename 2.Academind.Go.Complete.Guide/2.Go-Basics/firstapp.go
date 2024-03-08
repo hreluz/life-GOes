e89427df53f0b122ecdc0267891dd0cddc3fbcb9
Hector <hmreluz@gmail.com>
2024-03-08 01:16:13 -0500

    2.Academind Go Complete Guide: 2.go-basics: exploring variables

---
+++ 2.Academind.Go.Complete.Guide/2.Go-Basics/firstapp.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// var greetingText string
+	// greetingText = "Hello World"
+
+	// var greetingText string = "Heello world !"
+
+	greetingText := "Hello there"
+
+	fmt.Println(greetingText)
+	fmt.Println(greetingText)
+}
