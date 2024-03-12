58e640646d5c991605f1cac7c399bfb752459b8c
Hector <hmreluz@gmail.com>
2024-03-12 15:14:18 -0500

    2.Academind Go Complete Guide: 3.bmi: user input and cleaning/parsing inputs

---
+++ 2.Academind.Go.Complete.Guide/3.BMI/bmi.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+var reader = bufio.NewReader(os.Stdin)
+
+func main() {
+	fmt.Println("Hello there")
+	fmt.Println("-----------------------------")
+	fmt.Print("Please enter your weight (kg): ")
+	weightInput, _ := reader.ReadString('\n')
+
+	fmt.Print("Please enter your height (m): ")
+	heightInput, _ := reader.ReadString('\n') //this add a \n at the end
+
+	fmt.Print(weightInput)
+	fmt.Print(heightInput)
+
+	// Save that user input in variables and remove the \n
+	weightInput = strings.Replace(weightInput, "\n", "", -1)
+	heightInput = strings.Replace(heightInput, "\n", "", -1)
+
+	weight, _ := strconv.ParseFloat(weightInput, 64)
+	height, _ := strconv.ParseFloat(heightInput, 32)
+
+	fmt.Print(weight)
+	fmt.Print(height)
+
+}
