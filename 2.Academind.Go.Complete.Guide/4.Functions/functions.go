1f78ac11c071c5332383c4150c90db1304fbf820
Hector <hmreluz@gmail.com>
2024-03-12 17:55:46 -0500

    2.Academind Go Complete Guide: 4.functions: using named return values

--- 2.Academind.Go.Complete.Guide/4.Functions/functions.go
+++ 2.Academind.Go.Complete.Guide/4.Functions/functions.go
@@ -19,12 +19,15 @@ func main() {
 	fmt.Println("I'm executing inside a function")
 }
 
-func add(num1 int, num2 int) int {
-	return num1 + num2
+func add(num1 int, num2 int) (sum int) {
+	sum = num1 + num2
+	return
 }
 
-func generateRandomNumbers() (int, int) {
-	return rand.Intn(100), rand.Intn(40)
+func generateRandomNumbers() (r1 int, r2 int) {
+	r1 = rand.Intn(100)
+	r2 = rand.Intn(40)
+	return
 }
 
 func printNumber(number int) {
