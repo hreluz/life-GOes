0139f627e48356973c0ea924f5510288cc65061b
Hector <hmreluz@gmail.com>
2024-03-25 19:39:38 -0500

    2.Academind Go Complete Guide: 5.Functions on BMI: Outsourcing user input logic

--- 2.Academind.Go.Complete.Guide/3.BMI/bmi.go
+++ 2.Academind.Go.Complete.Guide/3.BMI/bmi.go
@@ -2,29 +2,16 @@ package main
 
 import (
 	"fmt"
-	"strconv"
-	"strings"
 	// found in mod when creating with go mod init
 	"github.com/hreluz/bmi/info"
 )
 
 func main() {
 	info.PrintWelcome()
-	fmt.Print(info.WeightPrompt)
-	weightInput, _ := reader.ReadString('\n')
 
-	fmt.Print(info.HeightInput)
-	heightInput, _ := reader.ReadString('\n') //this add a \n at the end
-
-	fmt.Print(weightInput)
-	fmt.Print(heightInput)
+	weight, height := getUserMetrics()
 
 	// Save that user input in variables and remove the \n
-	weightInput = strings.Replace(weightInput, "\n", "", -1)
-	heightInput = strings.Replace(heightInput, "\n", "", -1)
-
-	weight, _ := strconv.ParseFloat(weightInput, 64)
-	height, _ := strconv.ParseFloat(heightInput, 32)
 
 	// fmt.Print(weight)
 	// fmt.Print(height)
--- 2.Academind.Go.Complete.Guide/3.BMI/input.go
+++ 2.Academind.Go.Complete.Guide/3.BMI/input.go
@@ -2,7 +2,32 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"os"
+	"strconv"
+	"strings"
+
+	"github.com/hreluz/bmi/info"
 )
 
 var reader = bufio.NewReader(os.Stdin)
+
+func getUserMetrics() (weight float64, height float64) {
+
+	fmt.Print(info.WeightPrompt)
+	weightInput, _ := reader.ReadString('\n')
+
+	fmt.Print(info.HeightInput)
+	heightInput, _ := reader.ReadString('\n') //this add a \n at the end
+
+	fmt.Print(weightInput)
+	fmt.Print(heightInput)
+
+	weightInput = strings.Replace(weightInput, "\n", "", -1)
+	heightInput = strings.Replace(heightInput, "\n", "", -1)
+
+	weight, _ = strconv.ParseFloat(weightInput, 64)
+	height, _ = strconv.ParseFloat(heightInput, 32)
+
+	return
+}
