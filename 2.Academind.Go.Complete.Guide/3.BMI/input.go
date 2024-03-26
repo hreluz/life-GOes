7f014798d00e9bdfe2c4fb361a007a7355076b80
Hector <hmreluz@gmail.com>
2024-03-25 19:54:36 -0500

    2.Academind Go Complete Guide: 5.Functions on BMI: super refactor

--- 2.Academind.Go.Complete.Guide/3.BMI/input.go
+++ 2.Academind.Go.Complete.Guide/3.BMI/input.go
@@ -13,21 +13,17 @@ import (
 var reader = bufio.NewReader(os.Stdin)
 
 func getUserMetrics() (weight float64, height float64) {
-
-	fmt.Print(info.WeightPrompt)
-	weightInput, _ := reader.ReadString('\n')
-
-	fmt.Print(info.HeightInput)
-	heightInput, _ := reader.ReadString('\n') //this add a \n at the end
-
-	fmt.Print(weightInput)
-	fmt.Print(heightInput)
-
-	weightInput = strings.Replace(weightInput, "\n", "", -1)
-	heightInput = strings.Replace(heightInput, "\n", "", -1)
-
-	weight, _ = strconv.ParseFloat(weightInput, 64)
-	height, _ = strconv.ParseFloat(heightInput, 32)
-
+	weight = getUserInput(info.WeightPrompt)
+	height = getUserInput(info.HeightInput)
 	return
+}
+
+func getUserInput(propmText string) float64 {
+	fmt.Print(propmText)
+
+	userInput, _ := reader.ReadString('\n')
+	userInput = strings.Replace(userInput, "\n", "", -1)
+	value, _ := strconv.ParseFloat(userInput, 64)
+
+	return value
 }
