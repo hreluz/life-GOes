426b18f8eb0ff51ffc522f32017c0a8d9ef7dcd3
Hector <hmreluz@gmail.com>
2024-04-01 12:32:49 -0500

    2.Academind Go Complete Guide: 7.Structs: creating stuct for User fields

---
+++ 2.Academind.Go.Complete.Guide/6.Structs/structs.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"runtime"
+	"strings"
+	"time"
+)
+
+type User struct {
+	firstName   string
+	lastName    string
+	birthDate   string
+	createdDate time.Time
+}
+
+var reader = bufio.NewReader(os.Stdin)
+
+func main() {
+
+	var newUser User
+
+	firstName := getUserData("Please enter your first name: ")
+	lastName := getUserData("Please enter your last name: ")
+	birthdate := getUserData("Please enter your birthdate (MM/DD/YYYY): ")
+	created := time.Now()
+
+	// newUser = User{
+	// 	firstName:   firstName,
+	// 	lastName:    lastName,
+	// 	birthDate:   birthdate,
+	// 	createdDate: created,
+	// }
+
+	newUser = User{
+		firstName,
+		lastName,
+		birthdate,
+		created,
+	}
+	// fmt.Println(newUser.firstName, lastName, birthdate, created)
+	fmt.Println(newUser)
+}
+
+func getUserData(promptText string) string {
+	fmt.Print(promptText)
+	userInput, _ := reader.ReadString('\n')
+
+	var cleanedInput string
+
+	if runtime.GOOS == "windows" {
+		cleanedInput = strings.Replace(userInput, "\r\n", "", -1)
+	} else {
+		cleanedInput = strings.Replace(userInput, "\n", "", -1)
+	}
+	return cleanedInput
+}
