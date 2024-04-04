5171c2e007d13650f8dbfb7a2e6254045701c4f0
Hector <hmreluz@gmail.com>
2024-04-03 22:57:01 -0500

    2.Academind Go Complete Guide: 9.Array-slices and maps: dynamic lists with slices

--- 2.Academind.Go.Complete.Guide/8.Arrays-Slices-Maps/lists.go
+++ 2.Academind.Go.Complete.Guide/8.Arrays-Slices-Maps/lists.go
@@ -3,28 +3,42 @@ package main
 import "fmt"
 
 func main() {
-	// var productNames [4]string
-	var productNames [4]string = [4]string{"A book", "two books"}
-	productNames[2] = "A carpet"
+	// slices array
+	prices := []float64{10, 33}
+	fmt.Println(prices[0:1])
 
-	// arrays
-	prices := [4]float64{10.99, 9.33, 405.22, 23}
+	prices[1] = 9.99
 
-	fmt.Println(prices[3])
-	fmt.Println(productNames)
-
-	// slices reference to initial array
-	featuredPrices := prices[1:3]
-	highlightedPrices := featuredPrices[:1]
-	prices[1] = 34444
-	fmt.Println(featuredPrices)
-	fmt.Println(highlightedPrices)
-
-	// cap shows the capacity of the field
-	// capacity counts from the start element, but cannot go before that
-	fmt.Println(len(featuredPrices), cap(featuredPrices), cap(highlightedPrices))
-
-	highlightedPrices = highlightedPrices[:3]
-	fmt.Println(highlightedPrices)
-	fmt.Println(len(highlightedPrices), cap(featuredPrices), cap(highlightedPrices))
+	updatedPrices := append(prices, 5.34)
+	prices = prices[1:]
+	fmt.Println(updatedPrices)
+	fmt.Println(prices)
 }
+
+// func main() {
+// fixed length array
+// 	// var productNames [4]string
+// 	var productNames [4]string = [4]string{"A book", "two books"}
+// 	productNames[2] = "A carpet"
+
+// 	// arrays
+// 	prices := [4]float64{10.99, 9.33, 405.22, 23}
+
+// 	fmt.Println(prices[3])
+// 	fmt.Println(productNames)
+
+// 	// slices reference to initial array
+// 	featuredPrices := prices[1:3]
+// 	highlightedPrices := featuredPrices[:1]
+// 	prices[1] = 34444
+// 	fmt.Println(featuredPrices)
+// 	fmt.Println(highlightedPrices)
+
+// 	// cap shows the capacity of the field
+// 	// capacity counts from the start element, but cannot go before that
+// 	fmt.Println(len(featuredPrices), cap(featuredPrices), cap(highlightedPrices))
+
+// 	highlightedPrices = highlightedPrices[:3]
+// 	fmt.Println(highlightedPrices)
+// 	fmt.Println(len(highlightedPrices), cap(featuredPrices), cap(highlightedPrices))
+// }
