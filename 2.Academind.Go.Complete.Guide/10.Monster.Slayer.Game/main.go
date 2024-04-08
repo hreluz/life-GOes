cf4280bff7aee1972cf0567a74ffe2c51ccf95c1
Hector <hmreluz@gmail.com>
2024-04-08 15:33:29 -0500

    2.Academind Go Complete Guide: 11-Monster-Slayer-Game: menu choices

--- 2.Academind.Go.Complete.Guide/10.Monster.Slayer.Game/main.go
+++ 2.Academind.Go.Complete.Guide/10.Monster.Slayer.Game/main.go
@@ -1,4 +1,38 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/hreluz/monster-slayer-game/interaction"
+)
+
+var currentRound = 0
+
 func main() {
+	startGame()
+
+	winner := ""
+
+	for winner == "" {
+		executeRound()
+	}
+
+	endGame()
+}
+
+func startGame() {
+	interaction.PrintGreeting()
+}
+
+func executeRound() string {
+	currentRound++
+	isSpecialRound := currentRound%3 == 0
+	interaction.ShowAvailableActions(isSpecialRound)
+	userChoice := interaction.GetPlayerChoice(isSpecialRound)
+
+	fmt.Println(userChoice)
+	return userChoice
+}
+
+func endGame() {
 }
