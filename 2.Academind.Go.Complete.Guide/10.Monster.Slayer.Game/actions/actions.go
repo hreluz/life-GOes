9598e622ac48d3479373fa0d4b2bc7c5a7337000
Hector <hmreluz@gmail.com>
2024-04-08 16:05:21 -0500

    2.Academind Go Complete Guide: 11-Monster-Slayer-Game: logic for actions

--- 2.Academind.Go.Complete.Guide/10.Monster.Slayer.Game/actions/actions.go
+++ 2.Academind.Go.Complete.Guide/10.Monster.Slayer.Game/actions/actions.go
@@ -7,16 +7,44 @@ import (
 
 var randSource = rand.NewSource(time.Now().UnixNano())
 var randGenerator = rand.New(randSource)
+var currentMonsterHealth = 100
+var currentPlayerHealth = 100
 
 func AttackMonster(isSpecialAttack bool) {
+	minAttack := 5
+	maxAttack := 10
 
+	if isSpecialAttack {
+		minAttack = 10
+		maxAttack = 20
+	}
+
+	dmgValue := generateRandBetween(minAttack, maxAttack)
+	currentMonsterHealth -= dmgValue
 }
 
 func HealPlayer() {
+	minHealValue := 10
+
+	maxHealValue := 20
+
+	healValue := generateRandBetween(minHealValue, maxHealValue)
+
+	healthDiff := 100 - currentPlayerHealth
+
+	if healthDiff >= healValue {
+		currentPlayerHealth += healValue
+	} else {
+		currentPlayerHealth = 100
+	}
 }
 
 func AttackPlayer() {
+	minAttack := 9
+	maxAttack := 13
 
+	dmgValue := generateRandBetween(minAttack, maxAttack)
+	currentPlayerHealth -= dmgValue
 }
 
 func generateRandBetween(min int, max int) int {
