c86b9f9c2ed96614dfb6ca3346507a86d809b8a9
Hector <hmreluz@gmail.com>
2024-04-15 19:28:20 -0500

    2.Academind Go Complete Guide: 16.Go.routines - Transmiting values via channel

--- 2.Academind.Go.Complete.Guide/15.Goroutines/main.go
+++ 2.Academind.Go.Complete.Guide/15.Goroutines/main.go
@@ -2,56 +2,31 @@ package main
 
 import (
 	"fmt"
-	"os"
+	"math/rand"
+	"time"
 )
 
-func greet() {
-	fmt.Println("Hi there!")
-}
+var source = rand.NewSource(time.Now().Unix())
+var randN = rand.New(source)
 
-func storeData(storableText string, fileName string) {
-	file, err := os.OpenFile(fileName,
-		os.O_CREATE|os.O_APPEND|os.O_WRONLY,
-		0666,
-	)
+func generateValue(c chan int) {
+	sleepTime := randN.Intn(3)
+	time.Sleep(time.Duration(sleepTime) * time.Second)
 
-	if err != nil {
-		fmt.Println("Creating the file failed. Exiting")
-		return
-	}
-
-	defer file.Close()
-
-	_, err = file.WriteString(storableText)
-
-	if err != nil {
-		fmt.Println("Writing to the file failed")
-	}
-}
-
-func storeMoreData(lines int, fileName string, c chan int) {
-	for i := 0; i < lines; i++ {
-		text := fmt.Sprintf("Line %v - Dummy Data\n", i)
-		storeData(text, fileName)
-	}
-
-	fmt.Printf("-- Done storing %v lines of text --\n", lines)
-	c <- 1
+	c <- randN.Intn(10)
 }
 
 func main() {
-	greet()
-	storeData("This is some dummy data!", "dummy-data.txt")
 
-	channel := make(chan int)
+	c := make(chan int)
 
-	go storeMoreData(50000, "50000_1.txt", channel)
-	go storeMoreData(50000, "50000_2.txt", channel)
+	// running both generated numbers at the same time, rather than wait until the next one
+	go generateValue(c)
+	go generateValue(c)
 
-	fmt.Println("Program ends")
+	x := <-c
+	y := <-c
 
-	<-channel
-	<-channel
-
-	fmt.Println("Go routine ends")
+	sum := x + y
+	fmt.Println(sum)
 }
