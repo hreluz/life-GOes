aa91bcc271124d8640f9c503ff7e465447ee0ab7
Hector <hmreluz@gmail.com>
2024-04-15 19:43:09 -0500

    2.Academind Go Complete Guide: 16.Go.routines - using buffered channels (as chunks)

--- 2.Academind.Go.Complete.Guide/15.Goroutines/main.go
+++ 2.Academind.Go.Complete.Guide/15.Goroutines/main.go
@@ -9,23 +9,28 @@ import (
 var source = rand.NewSource(time.Now().Unix())
 var randN = rand.New(source)
 
-func generateValue(c chan int) {
+func generateValue(c chan int, limit chan int) {
+	limit <- 1
+	fmt.Println("Generating value...")
 	sleepTime := randN.Intn(3)
 	time.Sleep(time.Duration(sleepTime) * time.Second)
 
 	c <- randN.Intn(10)
+	<-limit
 }
 
 func main() {
 
 	c := make(chan int)
+	limiter := make(chan int, 3)
 
 	// running both generated numbers at the same time, rather than wait until the next one
-	go generateValue(c)
-	go generateValue(c)
-	go generateValue(c)
-	go generateValue(c)
-	go generateValue(c)
+	go generateValue(c, limiter)
+	go generateValue(c, limiter)
+	go generateValue(c, limiter)
+	go generateValue(c, limiter)
+	go generateValue(c, limiter)
+	go generateValue(c, limiter)
 
 	sum := 0
 	i := 0
@@ -34,7 +39,7 @@ func main() {
 		sum += num
 		i++
 
-		if i == 4 {
+		if i == 6 {
 			close(c)
 		}
 	}
