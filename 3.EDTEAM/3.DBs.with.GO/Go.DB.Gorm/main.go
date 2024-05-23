e629175d6eb6d0ad4f724621064ee27718e1927c
Hector <hmreluz@gmail.com>
2024-05-22 19:22:14 -0500

    3.EDTEAM - 3: DBs with ORM (Gorm) - CREATE

--- 3.EDTEAM/3.DBs.with.GO/Go.DB.Gorm/main.go
+++ 3.EDTEAM/3.DBs.with.GO/Go.DB.Gorm/main.go
@@ -6,12 +6,34 @@ import (
 )
 
 func main() {
-	driver := storage.MySQL
+	driver := storage.Postgres
 	storage.New(driver)
 
+	// Migrate
 	storage.DB().AutoMigrate(
 		&model.Product{},
 		&model.InvoiceHeader{},
 		&model.InvoiceItem{},
 	)
+
+	product1 := model.Product{
+		Name:  "Go Course",
+		Price: 88,
+	}
+
+	product2 := model.Product{
+		Name:  "Node Course",
+		Price: 98,
+	}
+
+	obs := "Testing with go"
+	product3 := model.Product{
+		Name:         "Testing Go",
+		Price:        33,
+		Observations: &obs,
+	}
+
+	storage.DB().Create(&product1)
+	storage.DB().Create(&product2)
+	storage.DB().Create(&product3)
 }
