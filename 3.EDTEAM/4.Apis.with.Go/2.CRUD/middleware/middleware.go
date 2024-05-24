0f2663b1c59e7b640207f4cdc598bade3b179cda
Hector <hmreluz@gmail.com>
2024-05-24 16:48:34 -0500

    3.EDTEAM - 4: APIS with Go - Middlewares - middleware to validate a token on request

--- 3.EDTEAM/4.Apis.with.Go/2.CRUD/middleware/middleware.go
+++ 3.EDTEAM/4.Apis.with.Go/2.CRUD/middleware/middleware.go
@@ -17,3 +17,23 @@ func Log(f func(http.ResponseWriter, *http.Request)) func(http.ResponseWriter, *
 		fmt.Printf("the request took: %fs\n", t2.Sub(t1).Seconds())
 	}
 }
+
+func Authentication(f func(http.ResponseWriter, *http.Request)) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		token := r.Header.Get("Authorization")
+
+		if token != "a-safe-token" {
+			forbidden(w, r)
+			return
+		}
+
+		f(w, r)
+	}
+}
+
+func forbidden(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusForbidden)
+	w.Write([]byte("You are not allowed"))
+
+}
