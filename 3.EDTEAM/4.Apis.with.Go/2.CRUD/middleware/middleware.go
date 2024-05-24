ae4baf5ad390cda70b071e6538ffc9e8376de0d7
Hector <hmreluz@gmail.com>
2024-05-24 16:17:39 -0500

    3.EDTEAM - 4: APIS with Go - Middlewares - middleware to check how long request took

--- 3.EDTEAM/4.Apis.with.Go/2.CRUD/middleware/middleware.go
+++ 3.EDTEAM/4.Apis.with.Go/2.CRUD/middleware/middleware.go
@@ -1,13 +1,19 @@
 package middleware
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"time"
 )
 
 func Log(f func(http.ResponseWriter, *http.Request)) func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
+		t1 := time.Now()
 		log.Printf("request %q, method: %q\n", r.URL.Path, r.Method)
 		f(w, r)
+		t2 := time.Now()
+
+		fmt.Printf("the request took: %fs\n", t2.Sub(t1).Seconds())
 	}
 }
