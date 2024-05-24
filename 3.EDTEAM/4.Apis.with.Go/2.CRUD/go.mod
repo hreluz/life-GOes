0e9c8893c950b1f30ce5edcb7736fdcd7eb19302
Hector <hmreluz@gmail.com>
2024-05-24 17:12:56 -0500

    3.EDTEAM - 4: APIS with Go - JWT AUTH - generate public and private certificate

--- 3.EDTEAM/4.Apis.with.Go/2.CRUD/go.mod
+++ 3.EDTEAM/4.Apis.with.Go/2.CRUD/go.mod
@@ -1,3 +1,5 @@
 module github.com/hreluz/go-api-crud
 
 go 1.22.0
+
+require github.com/golang-jwt/jwt/v5 v5.2.1 // indirect
---
+++ 3.EDTEAM/4.Apis.with.Go/2.CRUD/model/login.go
@@ -0,0 +1,15 @@
+package model
+
+import "github.com/golang-jwt/jwt/v5"
+
+// Login
+type Login struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+// Claim (body token)
+type Claim struct {
+	Email string `json:"email"`
+	jwt.Claims
+}
