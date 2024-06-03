79dee66bec764b488d15e9a01d0daaba4e43cf1c
Hector <hmreluz@gmail.com>
2024-06-03 17:15:14 -0500

    3.EDTEAM - 4: APIS with Go - ECHO PACKAGE - Create endpoint request with middleware recover

--- 3.EDTEAM/4.Apis.with.Go/4.Echo.Package/main.go
+++ 3.EDTEAM/4.Apis.with.Go/4.Echo.Package/main.go
@@ -2,16 +2,33 @@ package main
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
 	e := echo.New()
+	e.Use(middleware.Recover())
+
 	e.GET("/", hello)
+	e.GET("/division", division)
 	e.Start(":8080")
 }
 
 func hello(c echo.Context) error {
 	return c.JSON(http.StatusOK, map[string]string{"greeting": "Hello world"})
+}
+
+func division(c echo.Context) error {
+	d := c.QueryParam("d")
+	f, _ := strconv.Atoi(d)
+
+	if f == 0 {
+		return c.String(http.StatusBadRequest, "Value cannot be zero")
+	}
+
+	r := 3000 / f
+	return c.String(http.StatusOK, strconv.Itoa(r))
 }
