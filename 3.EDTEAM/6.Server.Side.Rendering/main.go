c302312b4fa3bf91ce5f5eb7f148831511631f9e
Hector <hmreluz@gmail.com>
2024-06-05 13:28:59 -0500

    3.EDTEAM - 5: Server Side Rendering - package text/template New, Parse and Execute

---
+++ 3.EDTEAM/6.Server.Side.Rendering/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"log"
+	"os"
+	"text/template"
+)
+
+func main() {
+	tpl01 := "Hello World"
+	t, err := template.New("exercise_1").Parse(tpl01)
+
+	if err != nil {
+		log.Fatalf("Error when doing parse on template %v", err)
+	}
+
+	err = t.Execute(os.Stdout, nil)
+
+	if err != nil {
+		log.Fatalf("Error when executing template %v", err)
+	}
+}
