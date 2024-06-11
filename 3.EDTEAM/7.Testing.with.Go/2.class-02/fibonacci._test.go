7130224efa0f5a9ca7bd0387db6c9b8ff4c851e1
Hector <hmreluz@gmail.com>
2024-06-11 18:20:04 -0500

    3.EDTEAM - 6: Testing with Go: 2.Logs, Errors: benchmark

---
+++ 3.EDTEAM/7.Testing.with.Go/2.class-02/fibonacci._test.go
@@ -0,0 +1,47 @@
+package class02
+
+import "testing"
+
+func TestFibonacciFor(t *testing.T) {
+	want := 55
+	got := fibonacciFor(10)
+	if got != want {
+		t.Errorf("It was expected %d, it got %d", want, got)
+	}
+}
+
+func TestFibonacciRec(t *testing.T) {
+	want := 55
+	got := fibonacciRec(10)
+	if got != want {
+		t.Errorf("It was expected %d, it got %d", want, got)
+	}
+}
+
+func TestFibonacciRecMem(t *testing.T) {
+	want := 55
+	got := fibonacciRecMem(10)
+	if got != want {
+		t.Errorf("It was expected %d, it got %d", want, got)
+	}
+}
+
+func BenchmarkFibonacciFor(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		fibonacciFor(30)
+	}
+}
+func BenchmarkFibonacciRec(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		fibonacciRec(30)
+	}
+}
+
+func BenchmarkFibonacciRecMem(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		fibonacciRecMem(30)
+	}
+}
+
+// run benchmark
+// go test -bench=.
