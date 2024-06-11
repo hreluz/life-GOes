a3919f68c1fae05ec1571f80a8d1c652b208c501
Hector <hmreluz@gmail.com>
2024-06-11 18:36:53 -0500

    3.EDTEAM - 6: Testing with Go: 3.Massive Test: Subtest, naming the tests

--- 3.EDTEAM/7.Testing.with.Go/3.class-03/multiply_test.go
+++ 3.EDTEAM/7.Testing.with.Go/3.class-03/multiply_test.go
@@ -5,21 +5,28 @@ import "testing"
 func TestMultiply(t *testing.T) {
 
 	table := []struct {
+		name string
 		a    int
 		b    int
 		want int
 	}{
-		{2, 1, 2},
-		{2, 2, 4},
-		{2, 3, 6},
-		{2, 4, 8},
+		{"2x1", 2, 1, 2},
+		{"2x2", 2, 2, 4},
+		{"2x3", 2, 3, 6},
+		{"2x4", 2, 4, 8},
 	}
 
 	for _, v := range table {
-		got := multiply(v.a, v.b)
+		t.Run(v.name, func(t *testing.T) {
+			got := multiply(v.a, v.b)
 
-		if got != v.want {
-			t.Errorf("Got %d, it was expected %d", got, v.want)
-		}
+			if got != v.want {
+				t.Errorf("Got %d, it was expected %d", got, v.want)
+			}
+		})
 	}
 }
+
+// sub test , naming the tests
+// go test -v
+// go test -v -run TestMultiply/2x3
