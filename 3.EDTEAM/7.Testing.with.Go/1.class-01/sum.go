915c86aa82d1f507789d2cabf80da9b0c38e630a
Hector <hmreluz@gmail.com>
2024-06-11 17:49:30 -0500

    3.EDTEAM - 6: Testing with Go: 1.Basic Concepts - second test and run specific test

--- 3.EDTEAM/7.Testing.with.Go/1.class-01/sum.go
+++ 3.EDTEAM/7.Testing.with.Go/1.class-01/sum.go
@@ -3,3 +3,14 @@ package class1
 func Add(a, b int) int {
 	return a + b
 }
+
+func AddMultiple(numbers ...int) int {
+
+	resp := 0
+
+	for _, v := range numbers {
+		resp += v
+	}
+
+	return resp
+}
--- 3.EDTEAM/7.Testing.with.Go/1.class-01/sum_test.go
+++ 3.EDTEAM/7.Testing.with.Go/1.class-01/sum_test.go
@@ -12,4 +12,17 @@ func TestAdd(t *testing.T) {
 	}
 }
 
+func TestAddMultiple(t *testing.T) {
+	want := 6
+	got := AddMultiple(2, 3, 1)
+
+	if got != want {
+		t.Logf("Error: It was expected %d, got %d", want, got)
+		t.Fail()
+	}
+}
+
 // go test
+
+// go test -run AddM
+// go test -run ple$
