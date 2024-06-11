8feeec3b9f06e744bcb5d6fb9def6d89fb894846
Hector <hmreluz@gmail.com>
2024-06-11 17:55:33 -0500

    3.EDTEAM - 6: Testing with Go: 2.Logs, Errors: show logs

--- 3.EDTEAM/7.Testing.with.Go/1.class-01/sum_test.go
+++ 3.EDTEAM/7.Testing.with.Go/1.class-01/sum_test.go
@@ -4,12 +4,15 @@ import "testing"
 
 func TestAdd(t *testing.T) {
 	want := 5
+	t.Logf("want value: %d\n", want)
 	got := Add(2, 3)
+	t.Logf("got value: %d\n", got)
 
 	if got != want {
 		t.Logf("Error: It was expected %d, got %d", want, got)
 		t.Fail()
 	}
+	t.Log("Add test finished")
 }
 
 func TestAddMultiple(t *testing.T) {
@@ -22,7 +25,13 @@ func TestAddMultiple(t *testing.T) {
 	}
 }
 
+// Run tests
+// ------------
 // go test
-
 // go test -run AddM
 // go test -run ple$
+
+// Show Logs
+// -------------
+// go test -v
+// go test -v -run AddM
