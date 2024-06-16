a37f7db34b49c32092f176521ce90b683d4ced5a
Hector <hmreluz@gmail.com>
2024-06-16 00:55:41 -0500

    3.EDTEAM - 7.Concurrency: sync access to memory: example with sync-mutex

--- 3.EDTEAM/8.Concurrency.with.Go/3.sync-mutex/main.go
+++ 3.EDTEAM/8.Concurrency.with.Go/3.sync-mutex/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
@@ -15,13 +16,22 @@ func (a *account) String() string {
 }
 
 func main() {
+	wg := sync.WaitGroup{}
+	mu := sync.Mutex{}
+	wg.Add(2)
 
 	ironman := account{name: "Ironman", balance: 500}
 	batman := account{name: "Batman", balance: 900}
 
 	for _, value := range []int{300, 300} {
-		transfer(value, &ironman, &batman)
+		go func(amount int) {
+			mu.Lock()
+			transfer(amount, &ironman, &batman)
+			mu.Unlock()
+			wg.Done()
+		}(value)
 	}
+	wg.Wait()
 }
 
 func transfer(amount int, source, dest *account) {
