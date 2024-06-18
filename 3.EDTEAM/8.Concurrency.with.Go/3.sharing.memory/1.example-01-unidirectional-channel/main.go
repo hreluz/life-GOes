c7e3e503a3c87bec684c6d338c920edcf40c59d5
Hector <hmreluz@gmail.com>
2024-06-18 10:18:28 -0500

    3.EDTEAM - 7.Concurrency: listening repeatly data from the channel with for range

--- 3.EDTEAM/8.Concurrency.with.Go/3.sharing.memory/1.example-01-unidirectional-channel/main.go
+++ 3.EDTEAM/8.Concurrency.with.Go/3.sharing.memory/1.example-01-unidirectional-channel/main.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	number := make(chan int, 2)
@@ -21,11 +23,15 @@ func send(number chan<- int) {
 }
 
 func receive(signal chan<- struct{}, number <-chan int) {
-	for i := 0; i < 3; i++ {
-		v, ok := <-number
-		if ok {
-			fmt.Printf("%d %t\n", v, ok)
-		}
+	// for i := 0; i < 10; i++ {
+	// 	v, ok := <-number
+	// 	if ok {
+	// 		fmt.Printf("%d %t\n", v, ok)
+	// 	}
+	// }
+
+	for v := range number {
+		fmt.Println(v)
 	}
 	// fmt.Println(<-number)
 	// fmt.Println(<-number)
