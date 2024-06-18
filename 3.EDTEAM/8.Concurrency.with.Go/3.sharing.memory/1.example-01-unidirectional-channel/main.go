1aabbcacd990c132f8fef87d28b288248e16046d
Hector <hmreluz@gmail.com>
2024-06-18 11:07:39 -0500

    3.EDTEAM - 7.Concurrency: multiple communication operations with the select sentence

--- 3.EDTEAM/8.Concurrency.with.Go/3.sharing.memory/1.example-01-unidirectional-channel/main.go
+++ 3.EDTEAM/8.Concurrency.with.Go/3.sharing.memory/1.example-01-unidirectional-channel/main.go
@@ -2,15 +2,22 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func main() {
-	number := make(chan int, 2)
-	signal := make(chan struct{})
-	go receive(signal, number)
-	send(number)
+	// number := make(chan int, 2)
+	// signal := make(chan struct{})
+	// go receive(signal, number)
+	// send(number)
 
-	<-signal
+	// <-signal
+
+	number3 := make(chan int)
+	signal3 := make(chan struct{})
+	go receive3(signal3, number3)
+	send3(number3)
+	signal3 <- struct{}{}
 }
 
 func send(number chan<- int) {
@@ -18,8 +25,30 @@ func send(number chan<- int) {
 	number <- 2
 	number <- 3
 	number <- 4
-
+	number <- 5
+	number <- 6
 	close(number)
+}
+
+func send3(number3 chan<- int) {
+	number3 <- 1
+	number3 <- 2
+	number3 <- 3
+	time.Sleep(1)
+	number3 <- 4
+}
+
+func receive3(signal3 <-chan struct{}, number3 <-chan int) {
+	for {
+		select {
+		case v := <-number3:
+			fmt.Println(v)
+		case <-signal3:
+			return
+		default:
+			fmt.Println("... waiting")
+		}
+	}
 }
 
 func receive(signal chan<- struct{}, number <-chan int) {
@@ -33,9 +62,6 @@ func receive(signal chan<- struct{}, number <-chan int) {
 	for v := range number {
 		fmt.Println(v)
 	}
-	// fmt.Println(<-number)
-	// fmt.Println(<-number)
-	// fmt.Println(<-number)
 
 	signal <- struct{}{}
 }
