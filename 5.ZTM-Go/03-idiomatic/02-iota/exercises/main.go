265ffa05f1b356cdc0e373b7ebb494d5ccc2d319
Hector <hmreluz@gmail.com>
2025-02-26 22:07:54 -0500

    5.ZTM GO - 03-idiomatic: iota: demo and exercises

---
+++ 5.ZTM-Go/03-idiomatic/02-iota/exercises/main.go
@@ -0,0 +1,58 @@
+//--Summary:
+//  Create a calculator that can perform basic mathematical operations.
+//
+//--Requirements:
+//* Mathematical operations must be defined as constants using iota
+//* Write a receiver function that performs the mathematical operation
+//  on two operands
+//* Operations required:
+//  - Add, Subtract, Multiply, Divide
+//* The existing function calls in main() represent the API and cannot be changed
+//
+//--Notes:
+//* Your program is complete when it compiles and prints the correct results
+
+package main
+
+import "fmt"
+
+type Operation byte
+
+const (
+	Add Operation = iota
+	Subtract
+	Multiply
+	Divide
+)
+
+func (o Operation) calculate(a, b int) int {
+
+	switch o {
+	case Add:
+		return a + b
+	case Subtract:
+		return a - b
+	case Multiply:
+		return a * b
+	case Divide:
+		return a / b
+	default:
+		return 0
+	}
+}
+
+func main() {
+
+	add := Add
+	sub := Subtract
+	mul := Multiply
+	div := Divide
+
+	fmt.Println(add.calculate(2, 2)) // = 4
+
+	fmt.Println(sub.calculate(10, 3)) // = 7
+
+	fmt.Println(mul.calculate(3, 3)) // = 9
+
+	fmt.Println(div.calculate(100, 2)) // = 50
+}
